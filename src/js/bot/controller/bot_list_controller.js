f2d69f63b88efb219945fba6399449226ab45483
Vincent Tertre <vincent.tertre@ticetime.com>
2014-04-29 02:58:36 +0200

    naive connection error handler

--- src/js/bot/controller/bot_list_controller.js
+++ src/js/bot/controller/bot_list_controller.js
@@ -3,6 +3,17 @@
 
   angular.module('bot')
     .controller('BotListController', ['$scope', 'Bots', function ($scope, Bots) {
-      $scope.bots = Bots.getAll();
+      $scope.errors = {
+        show: false,
+        content: 'Could not establish connection with remote API.'
+      };
+      $scope.bots = Bots.getAll(
+        function () {
+          $scope.errors.show = false;
+        },
+        function () {
+          $scope.errors.show = true;
+        }
+      );
     }]);
 })(angular);
