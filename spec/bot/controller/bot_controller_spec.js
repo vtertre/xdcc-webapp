af6803b881f714939e07f787b0b5716504602de7
Vincent Tertre <vincent.tertre@ticetime.com>
2014-04-30 14:00:57 +0200

    first naive unit tests

---
+++ spec/bot/controller/bot_controller_spec.js
@@ -0,0 +1,33 @@
+describe("BotController", function () {
+  'use strict';
+
+  var $scope, $routeParams, controller, bots, bot = {};
+
+  beforeEach(function () {
+    angular.mock.module('bot');
+  });
+
+  beforeEach(inject(function ($rootScope, $controller) {
+    $scope = $rootScope.$new();
+    $routeParams = {};
+    bots = jasmine.createSpyObj("resource", ['get']);
+    bots.get.andReturn(bot);
+    controller = $controller('BotController', {
+      $scope: $scope,
+      $routeParams: $routeParams,
+      Bots: bots
+    });
+  }));
+
+  it("is defined", function () {
+    expect(controller).toBeDefined();
+  });
+
+  it("asks the api for the selected bot", function () {
+    expect(bots.get).toHaveBeenCalled();
+  });
+
+  it("passes the bot to the view", function () {
+    expect($scope.bot).toEqual(bot);
+  });
+});
---
+++ spec/bot/controller/bot_list_controller_spec.js
@@ -0,0 +1,32 @@
+describe("BotListController", function () {
+  'use strict';
+
+  var $scope, controller, bots,
+    botCollection = [];
+
+  beforeEach(function () {
+    angular.mock.module('bot');
+  });
+
+  beforeEach(inject(function ($rootScope, $controller) {
+    $scope = $rootScope.$new();
+    bots = jasmine.createSpyObj("resource", ['getAll']);
+    bots.getAll.andReturn(botCollection);
+    controller = $controller('BotListController', {
+      $scope: $scope,
+      Bots: bots
+    });
+  }));
+
+  it("is defined", function () {
+    expect(controller).toBeDefined();
+  });
+
+  it("asks the api for the all the available bots", function () {
+    expect(bots.getAll).toHaveBeenCalled();
+  });
+
+  it("passes the bots to the view", function () {
+    expect($scope.bots).toEqual(botCollection);
+  });
+});
