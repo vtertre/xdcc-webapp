e95aadfb4403986fc356a053a27566f8af456177
Vincent Tertre <vincent.tertre@ticetime.com>
2014-04-30 14:31:20 +0200

    removed header

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ var i18n = require('i18next');
 
 exports.index = function (req, res) {
 
-	res.render('index', {
-        clazz : 'home',
-        title : i18n.t("app.title.global")});
+  res.render('index', {
+    clazz: 'home',
+    title: i18n.t("app.title.global")});
 };
