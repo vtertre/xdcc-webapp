8c3df0ae5e36b79a5afc7f96f93f3ee3a2b61043
Vincent Tertre <vincent.tertre@ticetime.com>
2014-10-27 23:19:00 +0100

    Refactoring

--- assets/js/bot/controller/bot_list_controller.js
+++ assets/js/bot/controller/bot_list_controller.js
@@ -6,31 +6,33 @@
 (function (angular) {
   'use strict';
 
+  function BotListController($scope, $route, Bots, botService) {
+    $scope.currentDate = new Date();
+    $scope.errors = {
+      show: false,
+      content: ''
+    };
+    $scope.bots = Bots.getAll(
+      // TODO Dunno how to test this
+      function () {
+        $scope.errors.show = false;
+      },
+      function () {
+        $scope.errors.content = 'Could not establish connection with remote API.';
+        $scope.errors.show = true;
+      }
+    );
+
+    $scope.refreshList = function () {
+      $route.reload();
+    };
+
+    $scope.hasNotBeenCheckedRecently = function (time) {
+      var timeRepresentation = botService.getTimeDiff(time, $scope.currentDate);
+      return (timeRepresentation.viable && timeRepresentation.hours >= 6);
+    };
+  }
+
   angular.module('bot')
-    .controller('BotListController', ['$scope', '$route', 'Bots', 'botService', function ($scope, $route, Bots, botService) {
-      $scope.currentDate = new Date();
-      $scope.errors = {
-        show: false,
-        content: ''
-      };
-      $scope.bots = Bots.getAll(
-        // TODO Dunno how to test this
-        function () {
-          $scope.errors.show = false;
-        },
-        function () {
-          $scope.errors.content = 'Could not establish connection with remote API.';
-          $scope.errors.show = true;
-        }
-      );
-
-      $scope.refreshList = function () {
-        $route.reload();
-      };
-
-      $scope.hasNotBeenCheckedRecently = function (time) {
-        var timeRepresentation = botService.getTimeDiff(time, $scope.currentDate);
-        return (timeRepresentation.viable && timeRepresentation.hours >= 6);
-      };
-    }]);
+    .controller('BotListController', ['$scope', '$route', 'Bots', 'botService', BotListController]);
 })(angular);
