528b5b524c8f9c6107a0b87f482a8eb8da3ebbab
Vincent Tertre <vincent.tertre@gmail.com>
2015-03-19 01:25:35 +0100

    update issuer/audience

--- routes/jwt_utilities.js
+++ routes/jwt_utilities.js
@@ -1,8 +1,8 @@
 module.exports = {
   config: {
     secret: process.env.JWT_SECRET,
-    audience: "xdcc-express-server",
-    issuer: "xdcc-api",
+    audience: "xdcc:express-server",
+    issuer: "xdcc:sessions",
     getToken: getTokenFromQueryString
   }
 };
