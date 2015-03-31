875cbfe5eb5f530e55a068c0b6492a49f99e19ed
Vincent Tertre <vincent.tertre@gmail.com>
2015-03-31 18:19:27 +0200

    refactor orchestrator controller spec

--- src/js/common/controller/orchestrator_controller_spec.js
+++ src/js/common/controller/orchestrator_controller_spec.js
@@ -5,32 +5,78 @@ var sinon = require("sinon");
 
 describe("OrchestratorController", function () {
 
-  var $scope, AUTH_EVENTS, AuthenticationService, Session, socket, controller;
+  var $scope, AUTH_EVENTS, AuthenticationService, Session, socket, user, controller;
 
   beforeEach(function () {
-    $scope = {
-      $on: sinon.spy()
-    };
-    Session = {
-      restoreIfAvailable: sinon.spy()
-    };
-    AUTH_EVENTS = {};
-    AuthenticationService = {};
-    socket = {};
-    var ConnectedUserController = require("./orchestrator_controller");
-    controller = new ConnectedUserController($scope, AUTH_EVENTS, AuthenticationService, Session, socket);
-  });
-
-  it("must be defined", function () {
-    expect(controller).to.be.defined;
-  });
-
-  it("must update the current user", function () {
-    var user = {
+    user = {
       id: "userId",
       login: "userLogin"
     };
-    $scope.setCurrentUser(user);
-    expect($scope.currentUser).to.deep.equal(user);
+    $scope = {
+      events: {},
+      $on: function (eventName, callback) {
+        $scope.events[eventName] = callback;
+      },
+      procEvent: function (eventName, event, arg) {
+        $scope.events[eventName].call(null, event, arg);
+      }
+    };
+    AUTH_EVENTS = {
+      logoutSuccess: "logoutSuccess"
+    };
+    AuthenticationService = {};
+    socket = {
+      emit: sinon.spy()
+    };
+  });
+
+  describe("whether or not there is an existing session", function () {
+    beforeEach(function () {
+      Session = {
+        restoreIfAvailable: sinon.stub().returns(null)
+      };
+      var ConnectedUserController = require("./orchestrator_controller");
+      controller = new ConnectedUserController($scope, AUTH_EVENTS, AuthenticationService, Session, socket);
+    });
+
+    it("must be defined", function () {
+      expect(controller).to.be.defined;
+    });
+
+    it("must try to restore the session when loading", function () {
+      expect(Session.restoreIfAvailable).to.have.been.called;
+    });
+
+    it("must update the current user", function () {
+      $scope.setCurrentUser(user);
+      expect($scope.currentUser).to.deep.equal(user);
+    });
+
+    it("must emit an event on the socket when the logout event is emitted on the scope", function () {
+      $scope.procEvent(AUTH_EVENTS.logoutSuccess, null, user.id);
+      expect(socket.emit).to.have.been.calledWith("userLoggedOut", user.id);
+    });
+  });
+
+  describe("with an existing session", function () {
+    beforeEach(function () {
+      Session = {
+        restoreIfAvailable: sinon.stub().returns(user)
+      };
+      var ConnectedUserController = require("./orchestrator_controller");
+      controller = new ConnectedUserController($scope, AUTH_EVENTS, AuthenticationService, Session, socket);
+    });
+
+    it("must be defined", function () {
+      expect(controller).to.be.defined;
+    });
+
+    it("must set the session user in the scope", function () {
+      expect($scope.currentUser).to.deep.equal(user);
+    });
+
+    it("must emit an event on the socket with some user info", function () {
+      expect(socket.emit).to.have.been.calledWith("restoreSession", user.id, user.login);
+    });
   });
 });
