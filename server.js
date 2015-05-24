72b1bfad216756bdacd4cfa741c6ca6ae767ff99
Vincent Tertre <vincent.tertre@gmail.com>
2015-05-24 11:42:20 +0200

    change xdcc-subtitles staging url

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ if ("staging" === app.get("env")) {
   revision.initMap(require("./public/genere/map.json"));
   app.locals.apiUrl = "https://xdcc-api.herokuapp.com";
   app.locals.sessionsAppUrl = "https://xdcc-sessions.herokuapp.com";
-  app.locals.subtitlesApiUrl = "https://xdcc-subtitles.herokuapp.com";
+  app.locals.subtitlesApiUrl = "http://subtitles.xdccdownloader.appspot.com";
 
   loginApp = require("./subapp/xdcc-login/app").app;
 }
