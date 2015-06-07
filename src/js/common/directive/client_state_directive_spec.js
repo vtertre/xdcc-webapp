b550dfbb823d8bd07c38846a3da85a9e0df7dd67
Vincent Tertre <vincent.tertre@gmail.com>
2015-06-07 20:37:25 +0200

    properly clean the handlers

--- src/js/common/directive/client_state_directive_spec.js
+++ src/js/common/directive/client_state_directive_spec.js
@@ -4,22 +4,37 @@ var expect = require("chai").use(require("sinon-chai")).expect;
 var sinon = require("sinon");
 
 describe("xdccClientState", function () {
-  var element, socket, directive;
+  var scope, element, socket, directive;
 
   beforeEach(function () {
+    scope = {
+      events: {},
+      $on: function (eventName, callback) {
+        scope.events[eventName] = callback;
+      },
+      trigger: function (eventName, arg) {
+        scope.events[eventName].call(null, arg);
+      }
+    };
     element = { css: sinon.spy() };
     socket = {
       events: {},
       on: function (eventName, callback) {
         socket.events[eventName] = callback;
       },
+      removeListener: function (eventName) {
+        delete socket.events[eventName];
+      },
       emit: function (eventName, arg) {
         socket.events[eventName].call(null, arg);
+      },
+      isListening: function (eventName) {
+        return socket.events[eventName] !== undefined;
       }
     };
     var XdccClientState = require("./client_state_directive");
     directive = new XdccClientState(socket);
-    directive.link(null, element);
+    directive.link(scope, element);
   });
 
   it("waits for the client to connect", function () {
@@ -30,5 +45,11 @@ describe("xdccClientState", function () {
   it("must inform the user when something is wrong with the client", function () {
     socket.emit("xdcc:clientError");
     expect(element.css).to.have.been.calledWith("color");
+  });
+
+  it("clears the handlers when the scope is destroyed", function () {
+    scope.trigger("$destroy");
+    expect(socket.isListening("xdcc:clientConnected")).to.be.false;
+    expect(socket.isListening("xdcc:clientError")).to.be.false;
   });
 });
