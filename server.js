08c454cb60173c1635917d1c60cc148c75ef45ab
Vincent Tertre <vincent.tertre@gmail.com>
2015-06-08 22:43:39 +0200

    let's try the new api in staging environment

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ if ("development" === app.get("env")) {
 
 if ("staging" === app.get("env")) {
   revision.initMap(require("./public/genere/map.json"));
-  app.locals.apiUrl = "https://xdcc-api.herokuapp.com";
+  app.locals.apiUrl = "http://api.xdccdownloader.appspot.com";
   app.locals.sessionsAppUrl = "https://xdcc-sessions.herokuapp.com";
   app.locals.subtitlesApiUrl = "http://subtitles.xdccdownloader.appspot.com";
 
