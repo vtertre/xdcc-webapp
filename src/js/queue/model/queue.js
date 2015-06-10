bd56441274eb1789d4c308dbf891e564d2dfda16
Vincent Tertre <vincent.tertre@gmail.com>
2015-06-10 16:04:52 +0200

    refactoring on queue model

--- src/js/queue/model/queue.js
+++ src/js/queue/model/queue.js
@@ -8,39 +8,39 @@ function Queue() {
   this.packIdQueue = [];
   this.packMap = {};
   this.length = 0;
-
-  this.push = function (pack) {
-    var packId = pack.botId + "_" + pack.position;
-    this.packIdQueue.push(packId);
-    this.packMap[packId] = pack;
-    ++this.length;
-  };
-
-  this.shift = function () {
-    var packId = this.packIdQueue.shift();
-    var pack = this.packMap[packId];
-    if (pack) {
-      delete this.packMap[packId];
-      --this.length;
-    }
-    return pack;
-  };
-
-  this.contains = function (pack) {
-    var packId = pack.botId + "_" + pack.position;
-    return !!(this.packMap[packId]);
-  };
-
-  this.remove = function (pack) {
-    var packId = pack.botId + "_" + pack.position;
-    var index = this.packIdQueue.indexOf(packId);
-    if (index !== -1) {
-      this.packIdQueue.splice(index, 1);
-      delete this.packMap[packId];
-      --this.length;
-    }
-  };
 }
+
+Queue.prototype.push = function (pack) {
+  var packId = pack.botId + "_" + pack.position;
+  this.packIdQueue.push(packId);
+  this.packMap[packId] = pack;
+  ++this.length;
+};
+
+Queue.prototype.shift = function () {
+  var packId = this.packIdQueue.shift();
+  var pack = this.packMap[packId];
+  if (pack) {
+    delete this.packMap[packId];
+    --this.length;
+  }
+  return pack;
+};
+
+Queue.prototype.contains = function (pack) {
+  var packId = pack.botId + "_" + pack.position;
+  return !!(this.packMap[packId]);
+};
+
+Queue.prototype.remove = function (pack) {
+  var packId = pack.botId + "_" + pack.position;
+  var index = this.packIdQueue.indexOf(packId);
+  if (index !== -1) {
+    this.packIdQueue.splice(index, 1);
+    delete this.packMap[packId];
+    --this.length;
+  }
+};
 
 Queue.prototype.getOrderedPacks = function () {
   var it = this;
